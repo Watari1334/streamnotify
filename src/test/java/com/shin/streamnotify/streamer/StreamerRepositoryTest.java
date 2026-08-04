1a8d2baecef97456bdab8564b7b678b360f4cac9
Watari <watari1334@gmail.com>
2026-08-04 14:51:50 +0900

    CI検証: StreamerRepositoryTestからflyway.clean()を削除

--- src/test/java/com/shin/streamnotify/streamer/StreamerRepositoryTest.java
+++ src/test/java/com/shin/streamnotify/streamer/StreamerRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.shin.streamnotify.streamer;
 
-import org.flywaydb.core.Flyway;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
@@ -17,15 +15,6 @@ class StreamerRepositoryTest {
 
     @Autowired
     private StreamerRepository streamerRepository;
-
-    @Autowired
-    private Flyway flyway;
-
-    @BeforeEach
-    void setUp() {
-        flyway.clean();
-        flyway.migrate();
-    }
 
     @Test
     void 配信者を保存して検索できる() {
