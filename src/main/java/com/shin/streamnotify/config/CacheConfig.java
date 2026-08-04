a507d2e01bd997fc4ecc8a6226bbfbfcc145e8b7
Watari <watari1334@gmail.com>
2026-08-04 14:46:50 +0900

    CI修正: EnableCachingをCacheConfigに集約しテストのダミーCacheManagerを削除

--- src/main/java/com/shin/streamnotify/config/CacheConfig.java
+++ src/main/java/com/shin/streamnotify/config/CacheConfig.java
@@ -2,6 +2,7 @@ package com.shin.streamnotify.config;
 
 import com.shin.streamnotify.twitch.TwitchEventSubService.ChannelSearchResult;
 import com.shin.streamnotify.youtube.YouTubeService;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 
 @Configuration
+@EnableCaching
 public class CacheConfig {
 
     @Bean
