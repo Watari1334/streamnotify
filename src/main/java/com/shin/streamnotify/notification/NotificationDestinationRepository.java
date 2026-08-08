239801b86a6780d0ab91eba5ae18c346dc10f221
Watari <watari1334@gmail.com>
2026-08-08 16:09:01 +0900

    NotificationDestinationRepositoryにJavadocを追加

--- src/main/java/com/shin/streamnotify/notification/NotificationDestinationRepository.java
+++ src/main/java/com/shin/streamnotify/notification/NotificationDestinationRepository.java
@@ -4,7 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * NotificationDestinationのリポジトリ。
+ * 1ユーザーにつき1件のみ登録される想定なので、findByUser_UserIdはOptionalで単一件を返す。
+ */
 public interface NotificationDestinationRepository extends JpaRepository<NotificationDestination, Long> {
 
+    /**
+     * 指定したユーザーの通知先を取得する。
+     *
+     * @param userId 対象ユーザーのID
+     * @return 登録済みの通知先。未登録の場合は空
+     */
     Optional<NotificationDestination> findByUser_UserId(Long userId);
 }
